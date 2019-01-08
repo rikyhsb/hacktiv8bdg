3c8b79a3febe1cd89d85f1d18d79b3d3c364a85d
Riky Hasibuan <riky.hasibuan@gmail.com>
2019-01-08 11:09:49 +0700

    Update Batch 2

---
+++ batch1.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Final Project Showcase Hacktiv8 FWDB Batch 1 Bandung</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+	<div class="preloader-background">
+		<div class="preloader-wrapper big active">
+			<div class="spinner-layer spinner-blue">
+			    <div class="circle-clipper left">
+		      		<div class="circle"></div>
+			    </div>
+			    <div class="gap-patch">
+			      <div class="circle"></div>
+			    </div>
+			    <div class="circle-clipper right">
+			      <div class="circle"></div>
+			    </div>
+			</div>
+
+      		<div class="spinner-layer spinner-red">
+      			<div class="circle-clipper left">
+		          <div class="circle"></div>
+		        </div>
+		        <div class="gap-patch">
+		          <div class="circle"></div>
+		        </div>
+		        <div class="circle-clipper right">
+		          <div class="circle"></div>
+		        </div>
+		    </div>
+
+		    <div class="spinner-layer spinner-yellow">
+		    	<div class="circle-clipper left">
+		          <div class="circle"></div>
+		        </div>
+		        <div class="gap-patch">
+		          <div class="circle"></div>
+		        </div>
+		        <div class="circle-clipper right">
+		          <div class="circle"></div>
+		        </div>
+		    </div>
+
+	      	<div class="spinner-layer spinner-green">
+	        	<div class="circle-clipper left">
+	          		<div class="circle"></div>
+	        	</div>
+	        	<div class="gap-patch">
+	          		<div class="circle"></div>
+	        	</div>
+	        	<div class="circle-clipper right">
+	          		<div class="circle"></div>
+	        	</div>
+	      	</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col s12">
+				<div class="center-align" style="margin-top:25px; margin-bottom: 25px;">
+				  	<img src="https://hacktiv8.com/img/logo-hacktiv8_bordered.png">
+				  	<div class="clearfix">
+			  		<span style="color: #444; font-size:47px; font-family: 'Lato' !important; font-weight: 400;">Final Project Showcase</span>
+			  		<br>
+			  		<span style="color: #444; font-size:20px; font-family: 'Lato' !important; font-weight: 400;">Front End Web Development Basics Batch 1 Bandung</span>
+				</div>
+			</div>
+		</div> 
+		<div class="row">
+			<div class="col s3">
+				<div class="card sticky-action">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="img/cecep.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Cecep Triyandi</span>
+				    </div>
+				    <div class="card-reveal">
+						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+						<p>
+							About : <br> <b>Enterpreuner</b>
+							<div class="clearfix"></div>
+							Final Project : <br> <b>Photography Enthusiast</b>
+							<div class="clearfix"></div>
+							<div class="linkproject">
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://ctriyandiputra.netlify.com/" target="_blank"><i class="material-icons right">link</i></a>
+							</div>
+						</p>
+				    </div>
+			 	</div>
+			</div>
+			<div class="col s3">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="img/luthfi.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Luthfi Azizi</span>
+				    </div>
+				    <div class="card-reveal">
+						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+						<p>
+							About : <br> <b>Finance</b>
+							<div class="clearfix"></div>
+							Final Project : <br> <b>Web Company - Aksata Atelier</b>
+							<div class="clearfix"></div>
+							<div class="linkproject">
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://aksataetelier.netlify.com/" target="_blank"><i class="material-icons right">link</i></a>
+							</div>
+						</p>
+				    </div>
+			 	</div>
+			</div>
+			<div class="col s3">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="img/hanna.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Liqahanna Aviena P</span>
+				    </div>
+				   	<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+						<p>
+							About : <br> <b>Saya Makhluk Hidup</b>
+							<div class="clearfix"></div>
+							Final Project : <br> <b>Kucing Terbang Studio</b>
+							<div class="clearfix"></div>
+							<div class="linkproject">
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://kucing-terbang.netlify.com/" target="_blank"><i class="material-icons right">link</i></a>
+							</div>
+						</p>
+				    </div>
+			 	</div>
+			</div>
+
+			<div class="col s3">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="img/andriyana.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Andriyana</span>
+				    </div>
+				    <div class="card-reveal">
+						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+						<p>
+							About : <br> <b>Creative Officer</b>
+							<div class="clearfix"></div>
+							Final Project : <br> <b>Cilsy Career Landing Page</b>
+							<div class="clearfix"></div>
+							<div class="linkproject">
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://romantic-volhard-839851.netlify.com/" target="_blank"><i class="material-icons right">link</i></a>
+							</div>
+						</p>
+				    </div>
+			 	</div>
+			</div>
+
+			<div class="col s3">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="img/regi.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Regi Rivaldi</span>
+				    </div>
+				    <div class="card-reveal">
+						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+						<p>
+							About : <br> <b>International Law</b>
+							<div class="clearfix"></div>
+							Final Project : <br> <b>Travel Agency - MauKemana</b>
+							<div class="clearfix"></div>
+							<div class="linkproject">
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://maukemana.netlify.com/" target="_blank"><i class="material-icons right">link</i></a>
+							</div>
+						</p>
+				    </div>
+			 	</div>
+			</div>
+
+			<div class="col s3">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="img/geeraldo.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Geraldo Jose</span>
+				    </div>
+				    <div class="card-reveal">
+						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+						<p>
+							About : <br> <b>Mahasiswa</b>
+							<div class="clearfix"></div>
+							Final Project : <br> <b>Web Showroom Mobil</b>
+							<div class="clearfix"></div>
+							<div class="linkproject">
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://geeraldojoseshowroom.netlify.com/" target="_blank"><i class="material-icons right">link</i></a>
+							</div>
+						</p>
+				    </div>
+			 	</div>
+			</div>
+
+			<div class="col s3">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="img/prima.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Claudia C Primadani</span>
+				    </div>
+				    <div class="card-reveal">
+						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+						<p>
+							About : <br> <b>Mahasiswa</b>
+							<div class="clearfix"></div>
+							Final Project : <br> <b>Software House Landing Page</b>
+							<div class="clearfix"></div>
+							<div class="linkproject">
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://primakashi.netlify.com/" target="_blank"><i class="material-icons right">link</i></a>
+							</div>
+						</p>
+				    </div>
+			 	</div>
+			</div>
+
+			<div class="col s3">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="img/krisna.jpg">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Krishna Diswatra</span>
+				    </div>
+				    <div class="card-reveal">
+						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+						<p>
+							About : <br> <b>Graphic Designer</b>
+							<div class="clearfix"></div>
+							Final Project : <br> <b>Krishna Diswatra Photography Portfolio Website</b>
+							<div class="clearfix"></div>
+							<div class="linkproject">
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://krishnadiswatraphotography.netlify.com" target="_blank"><i class="material-icons right">link</i></a>
+							</div>
+						</p>
+				    </div>
+			 	</div>
+			</div>
+		</div>
+	</div>
+
+	<footer style="color: white; background-color: #222; text-align: center; padding: 5px">Hacktiv8 FWDB Batch 1 | CO&CO Space, Bandung
+	</footer>
+</body>
+</html>
--- batch2.php
+++ batch2.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Final Project Showcase Hacktiv8 FWDB Bandung</title>
+	<title>Final Project Showcase Hacktiv8 FWDB Batch 2 Bandung</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
@@ -75,50 +75,69 @@
 				  	<div class="clearfix">
 			  		<span style="color: #444; font-size:47px; font-family: 'Lato' !important; font-weight: 400;">Final Project Showcase</span>
 			  		<br>
-			  		<span style="color: #444; font-size:20px; font-family: 'Lato' !important; font-weight: 400;">Front End Web Development Basics Bandung</span>
+			  		<span style="color: #444; font-size:20px; font-family: 'Lato' !important; font-weight: 400;">Front End Web Development Basics Batch 2 Bandung</span>
 				</div>
 			</div>
 		</div> 
 		<div class="row">
-			<div class="col s6">
+			<div class="col s4">
 				<div class="card sticky-action">
 				    <div class="card-image waves-effect waves-block waves-light">
-				      <img class="activator" src="img/card.png">
+				      <img class="activator" src="img/laras.png">
 				    </div>
 				    <div class="card-content">
-				      <span class="card-title activator grey-text text-darken-4">FWDB Batch 1</span>
+				      <span class="card-title activator grey-text text-darken-4">Fadiah Putri Larasati</span>
 				    </div>
 				    <div class="card-reveal">
 						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
 						<p>
 							<div class="clearfix"></div>
-							<b>Batch 1</b>
+							Final Project : <br> <b>Fapularas Portfolio</b>
 							<div class="clearfix"></div>
-							Location : <br><b>CO&CO Space Jl. Dipati Ukur No.5, Lebakgede, Coblong, Kota Bandung, Jawa Barat 40132</b>
 							<div class="linkproject">
-								<a class="waves-effect waves-light btn-floating btn orange right" href="batch1.php"><i class="material-icons right">link</i></a>
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://fapularass.netlify.com/" target="_blank"><i class="material-icons right">link</i></a>
 							</div>
 						</p>
 				    </div>
 			 	</div>
 			</div>
-			<div class="col s6">
+			<div class="col s4">
 				<div class="card">
 				    <div class="card-image waves-effect waves-block waves-light">
-				      <img class="activator" src="img/card.png">
+				      <img class="activator" src="img/rama.jpg">
 				    </div>
 				    <div class="card-content">
-				      <span class="card-title activator grey-text text-darken-4">FWDB Batch 2</span>
+				      <span class="card-title activator grey-text text-darken-4">Nur Ramadani Subagja</span>
 				    </div>
 				    <div class="card-reveal">
 						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
 						<p>
 							<div class="clearfix"></div>
-							<b>Batch 2</b>
+							Final Project : <br> <b>Culture Co-Working Space</b>
 							<div class="clearfix"></div>
-							Location : <br><b>CO&CO Space Jl. Dipati Ukur No.5, Lebakgede, Coblong, Kota Bandung, Jawa Barat 40132</b>
 							<div class="linkproject">
-								<a class="waves-effect waves-light btn-floating btn orange right" href="batch2.php"><i class="material-icons right">link</i></a>
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://cuturews.netlify.com/" target="_blank"><i class="material-icons right">link</i></a>
+							</div>
+						</p>
+				    </div>
+			 	</div>
+			</div>
+			<div class="col s4">
+				<div class="card">
+				    <div class="card-image waves-effect waves-block waves-light">
+				      <img class="activator" src="img/sherilyn.png">
+				    </div>
+				    <div class="card-content">
+				      <span class="card-title activator grey-text text-darken-4">Sherilyn Silviani</span>
+				    </div>
+				   	<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+						<p>
+							<div class="clearfix"></div>
+							Final Project : <br> <b>Personal Website</b>
+							<div class="clearfix"></div>
+							<div class="linkproject">
+								<a class="waves-effect waves-light btn-floating btn orange right" href="https://sherilynslvn.netlify.com/" target="_blank"><i class="material-icons right">link</i></a>
 							</div>
 						</p>
 				    </div>
@@ -127,7 +146,7 @@
 		</div>
 	</div>
 
-	<footer style="color: white; background-color: #222; text-align: center; padding: 5px">Hacktiv8 FWDB Bandung
+	<footer style="color: white; background-color: #222; text-align: center; padding: 5px">Hacktiv8 FWDB Batch 2 | CO&CO Space, Bandung
 	</footer>
 </body>
 </html>
